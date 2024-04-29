d047b64a3074c70d3f8debf6419999695a670c87
Shashank Chittapur <shashank.chittapur@solibri.com>
2024-04-29 19:07:05 +0530

    Issue #1: Create bundle

--- __tests__/genrate-summary.test.ts
+++ __tests__/genrate-summary.test.ts
@@ -1,18 +1,16 @@
 import { expect } from '@jest/globals'
 import generateSummary from '../src/generate-summary'
 
-describe('generate-summary.ts',() => {
- 
-    it('validate summary report',async () => {
-    
-        const reportPath = '__tests__/resources'
-        const summary = await generateSummary(reportPath)
-        expect(summary.failedTests.length).toBe(5)
-        expect(summary.passedTests.length).toBe(52)
-        expect(summary.statistics.pass).toBe(52)
-        expect(summary.statistics.fail).toBe(5)
-        expect(summary.statistics.skip).toBe(0)
-        expect(summary.total).toBe(57)
-        expect(summary.passPercentage).toBe(91.23)
-    })
+describe('generate-summary.ts', () => {
+  it('validate summary report', async () => {
+    const reportPath = '__tests__/resources'
+    const summary = await generateSummary(reportPath)
+    expect(summary.failedTests.length).toBe(5)
+    expect(summary.passedTests.length).toBe(52)
+    expect(summary.statistics.pass).toBe(52)
+    expect(summary.statistics.fail).toBe(5)
+    expect(summary.statistics.skip).toBe(0)
+    expect(summary.total).toBe(57)
+    expect(summary.passPercentage).toBe(91.23)
+  })
 })
--- src/main.ts
+++ src/main.ts
@@ -12,15 +12,15 @@ export async function run(): Promise<void> {
       token: core.getInput('gh_access_token'),
       report_path: core.getInput('report_path'),
       sha: core.getInput('sha'),
-      pull_request_id: core.getInput('pull_request_id'),
+      pull_request_id: core.getInput('pull_request_id')
     }
 
-    if(!inputs.report_path) {
+    if (!inputs.report_path) {
       core.setFailed('report_path is required')
       return
     }
 
-    if(!inputs.sha) {
+    if (!inputs.sha) {
       core.setFailed('sha is required')
       return
     }
@@ -28,53 +28,88 @@ export async function run(): Promise<void> {
     const owner = process.env.GITHUB_REPOSITORY?.split('/')[0]
     core.debug(`owner: ${owner}`)
 
-    const summary: GenerateSummaryReport  = await generateSummary(inputs.report_path)
+    const summary: GenerateSummaryReport = await generateSummary(
+      inputs.report_path
+    )
 
     await core.summary
-                .addHeading("Robot Framework Test Report")
-                .addBreak()
-                .addHeading("Robot Results Summary")
-                .addTable([
-                  [{data:'Passed ✅', header:true}, {data:'Failed ❌', header:true}, {data:'Skipped :rocket: ', header:true},{data:'Total', header:true},{data:'Pass%', header:true}],
-                  [String(summary.statistics.pass), String(summary.statistics.fail), String(summary.statistics.skip), String(summary.statistics.pass + summary.statistics.fail + summary.statistics.skip), (summary.statistics.pass/(summary.statistics.pass + summary.statistics.fail + summary.statistics.skip)*100).toFixed(2)]
-                ])
-                .addBreak()
-                .addHeading("Failed Tests")
-                .addTable([
-                  [{data:'Test Name', header:true}, {data:'Execution Time :clock:', header:true}, {data:'Message', header:true}],
-                  ...summary.failedTests.map(test => [test.name, `${test.execution_time.toFixed(2)} s` , test.message])
-                ])
-                .write();
+      .addHeading('Robot Framework Test Report')
+      .addBreak()
+      .addHeading('Robot Results Summary')
+      .addTable([
+        [
+          { data: 'Passed ✅', header: true },
+          { data: 'Failed ❌', header: true },
+          { data: 'Skipped :rocket: ', header: true },
+          { data: 'Total', header: true },
+          { data: 'Pass%', header: true }
+        ],
+        [
+          String(summary.statistics.pass),
+          String(summary.statistics.fail),
+          String(summary.statistics.skip),
+          String(
+            summary.statistics.pass +
+              summary.statistics.fail +
+              summary.statistics.skip
+          ),
+          (
+            (summary.statistics.pass /
+              (summary.statistics.pass +
+                summary.statistics.fail +
+                summary.statistics.skip)) *
+            100
+          ).toFixed(2)
+        ]
+      ])
+      .addBreak()
+      .addHeading('Failed Tests')
+      .addTable([
+        [
+          { data: 'Test Name', header: true },
+          { data: 'Execution Time :clock:', header: true },
+          { data: 'Message', header: true }
+        ],
+        ...summary.failedTests.map(test => [
+          test.name,
+          `${test.execution_time.toFixed(2)} s`,
+          test.message
+        ])
+      ])
+      .write()
 
-      // Comment on the PR with the summary as a comment
-      if (inputs.pull_request_id && inputs.token) {
-        const octokit = require('@octokit/rest')({
-          auth: inputs.token
-        });
+    // Comment on the PR with the summary as a comment
+    if (inputs.pull_request_id && inputs.token) {
+      const octokit = require('@octokit/rest')({
+        auth: inputs.token
+      })
 
-        const summaryStatistics = summary.statistics;
-        const summaryFailedTests = summary.failedTests;
+      const summaryStatistics = summary.statistics
+      const summaryFailedTests = summary.failedTests
 
-        const comment =
-          'Robot Framework Test Report\n\n' +
-          'Robot Results Summary\n\n' +
-          '| Passed | Failed | Skipped | Total | Pass% |\n' +
-          '| --- | --- | --- | --- | --- |\n' +
-          `| ${summaryStatistics.pass} | ${summaryStatistics.fail} | ${summaryStatistics.skip} | ${summaryStatistics.pass + summaryStatistics.fail + summaryStatistics.skip} | ${(summaryStatistics.pass / (summaryStatistics.pass + summaryStatistics.fail + summaryStatistics.skip) * 100).toFixed(2)} |\n\n` +
-          'Failed Tests\n\n' +
-          '| Test Name | Execution Time | Message |\n' +
-          '| --- | --- | --- |\n' +
-          summaryFailedTests.map(test => `| ${test.name} | ${test.execution_time} | ${test.message} |\n`).join('\n');
+      const comment =
+        'Robot Framework Test Report\n\n' +
+        'Robot Results Summary\n\n' +
+        '| Passed | Failed | Skipped | Total | Pass% |\n' +
+        '| --- | --- | --- | --- | --- |\n' +
+        `| ${summaryStatistics.pass} | ${summaryStatistics.fail} | ${summaryStatistics.skip} | ${summaryStatistics.pass + summaryStatistics.fail + summaryStatistics.skip} | ${((summaryStatistics.pass / (summaryStatistics.pass + summaryStatistics.fail + summaryStatistics.skip)) * 100).toFixed(2)} |\n\n` +
+        'Failed Tests\n\n' +
+        '| Test Name | Execution Time | Message |\n' +
+        '| --- | --- | --- |\n' +
+        summaryFailedTests
+          .map(
+            test =>
+              `| ${test.name} | ${test.execution_time} | ${test.message} |\n`
+          )
+          .join('\n')
 
-        await octokit.issues.createComment({
-          owner,
-          repo: process.env.GITHUB_REPOSITORY?.split('/')[1],
-          issue_number: parseInt(inputs.pull_request_id),
-          body: comment
-        });
-      }
-
-
+      await octokit.issues.createComment({
+        owner,
+        repo: process.env.GITHUB_REPOSITORY?.split('/')[1],
+        issue_number: parseInt(inputs.pull_request_id),
+        body: comment
+      })
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
@@ -83,9 +118,9 @@ export async function run(): Promise<void> {
 
 export interface Inputs {
   token: string
-  report_path:string
-  sha:string
-  pull_request_id:string
+  report_path: string
+  sha: string
+  pull_request_id: string
 }
 
 export interface Test {
@@ -93,11 +128,11 @@ export interface Test {
   status: string
   suite: string
   execution_time: number
-  message: string 
+  message: string
 }
 
 export interface Statistics {
-   pass: number
-   fail: number
-   skip: number
+  pass: number
+  fail: number
+  skip: number
 }
